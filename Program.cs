ba1632a650ffe6bcbbb4c00447a0d988a8b68dc8
Denis Matvejevs <gradash@gmail.com>
2020-02-24 13:53:58 +0200

    c# start

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+
+            Console.WriteLine("age: ");
+            string text = Console.ReadLine();
+            int a = Convert.ToInt32(text);
+            if (a < 18)
+            {
+                Console.WriteLine("too young");
+            }
+            else
+            {
+                Console.WriteLine("too old");
+            }
+            Console.ReadKey();
+        }
+    }
+}
